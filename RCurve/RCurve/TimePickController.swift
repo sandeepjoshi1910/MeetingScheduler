c630960696ef5b01da1015863f16e76dd05b3595
Sandeep Joshi <aw.snjoshi@hotmail.com>
2019-03-02 14:50:28 -0600

    CleanUp

--- RCurve/RCurve/TimePickController.swift
+++ RCurve/RCurve/TimePickController.swift
@@ -31,8 +31,6 @@ class TimePickController: UIViewController {
         }
         
         
-        
-        // Do any additional setup after loading the view.
     }
     
 
@@ -48,15 +46,6 @@ class TimePickController: UIViewController {
         present(self.meetingScheduler, animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
