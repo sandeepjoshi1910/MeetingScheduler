2757f46a37dcf60605044f462d6a358fc7c27c26
Sandeep Joshi <aw.snjoshi@hotmail.com>
2019-03-02 16:32:28 -0600

    Building Time Zone Picker VC

--- RCurve/RCurve/TimePickController.swift
+++ RCurve/RCurve/TimePickController.swift
@@ -10,27 +10,58 @@ import UIKit
 import CoreLocation
 import TimeZonePicker
 
-class TimePickController: UIViewController {
-
+class TimePickController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     var timeDiff = 0
     var meetingScheduler : ViewController = ViewController()
+    
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var addTimeZoneBtn: UIButton!
+    @IBOutlet weak var timeZoneCV: UICollectionView!
+    var timeZones : [TimeZone] = []
     
     @IBOutlet weak var loclabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString("1021 S Racine Ave. Chicago") { (placemarks, error) in
-            let placemark = placemarks?.first
-            let lat = placemark?.location?.coordinate.latitude
-            let lon = placemark?.location?.coordinate.longitude
-            print("The lattitude : \(String(describing: lat)) and Longitude \(String(describing: lon))")
-            self.loclabel.text = "The lattitude : \(lat!) and Longitude \(lon!)"
-            let location = CLLocation(latitude: lat!, longitude: lon!)
-            self.loclabel.text = "\(location.timestamp)"
+        // Collection View Setup
+        timeZoneCV.register(UINib(nibName: "TimeZoneCell", bundle: nil), forCellWithReuseIdentifier: "timezoneview")
+        timeZoneCV.dataSource = self
+        timeZoneCV.delegate = self
+        timeZoneCV.contentInset = UIEdgeInsets(top: 20.0, left: 0, bottom: 20.0, right: 0)
+        
+        self.setupButtons()
+        
+    }
+    
+    func setupButtons() {
+        let btns : [UIButton] = [self.backBtn,self.addTimeZoneBtn,self.doneBtn]
+        for btn in btns {
+            btn.layer.borderColor = UIColor.blue.cgColor
+            btn.layer.borderWidth = 0.5
+            btn.layer.cornerRadius = 4.0
         }
-        
-        
+        self.doneBtn.isEnabled = false
+        self.doneBtn.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    // MARK : TimeZone Collection View
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.timeZones.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = timeZoneCV.dequeueReusableCell(withReuseIdentifier: "timezoneview", for: indexPath) as! TimeZoneCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 15.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15.0
     }
     
 
@@ -46,13 +77,31 @@ class TimePickController: UIViewController {
         present(self.meetingScheduler, animated: true, completion: nil)
     }
     
-
+    
+    @IBAction func addTimeZone(_ sender: Any) {
+        let timeZonePicker = TimeZonePickerViewController.getVC(withDelegate: self)
+        present(timeZonePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func backClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 extension TimePickController: TimeZonePickerDelegate {
     
     func timeZonePicker(_ timeZonePicker: TimeZonePickerViewController, didSelectTimeZone timeZone: TimeZone) {
         self.loclabel.text = "\(timeZone.identifier) \(timeZone.abbreviation()!)"
+        self.timeZones.append(timeZone)
+        self.timeZoneCV.reloadData()
+        
+        if self.timeZones.count == 2 {
+            self.addTimeZoneBtn.isEnabled = false
+            self.addTimeZoneBtn.layer.borderColor = UIColor.lightGray.cgColor
+            self.doneBtn.isEnabled = true
+            self.doneBtn.layer.borderColor = UIColor.blue.cgColor
+        }
 //        timeZoneOffset.text = timeZone.abbreviation()
         self.timeDiff = timeZone.secondsFromGMT() - self.timeDiff
         timeZonePicker.dismiss(animated: true, completion: nil)
--- RCurve/RCurve/TimeZoneCell.swift
+++ RCurve/RCurve/TimeZoneCell.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class TimeZoneCell: UICollectionViewCell {
+    
+    @IBOutlet weak var removeBtn: UIButton!
+    
+    @IBOutlet weak var locationName: UILabel!
+    
+    @IBOutlet weak var timeZoneText: UILabel!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        layer.shadowColor = UIColor.lightGray.cgColor
-        layer.shadowRadius = 12.0
-        layer.shadowOpacity = 0.7
-        layer.shadowOffset = CGSize.zero
-        layer.cornerRadius = 20.0
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowRadius = 6.0
+        self.layer.shadowOpacity = 0.4
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.cornerRadius = 15.0
+
     }
+    
+    @IBAction func removeTImeZone(_ sender: Any) {
+        
+        
+    }
+    
 
 }
